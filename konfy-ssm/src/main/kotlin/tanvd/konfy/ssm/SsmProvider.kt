a7921ecba73ea7cea9cb458b351f24a1e092ac0f
vdtankov <vdtankov@gmail.com>
2019-06-05 16:36:49 +0300

    Add separator mapping for SSM provider

--- konfy-ssm/src/main/kotlin/tanvd/konfy/ssm/SsmProvider.kt
+++ konfy-ssm/src/main/kotlin/tanvd/konfy/ssm/SsmProvider.kt
@@ -9,14 +9,17 @@ import java.lang.reflect.Type
 
 /**
  * Provider takes values from AWS SSM.
- * Each key will be prepended with a prefix (with a dot after it) -- "${prefix}.${key}"
+ * Each key will be prepended with a prefix (with a slash after it) -- "${prefix}/${key}"
  * If prefix is null, then key itself will be used as key
+ * @param prefix prefix for SSM
+ * @param separator separator used by a client, provider will map it to `/` 
  */
 class SsmProvider(private val prefix: String?, private val ssm: AWSSimpleSystemsManagement = SsmClient.defaultClient,
+                  private val separator: String,
                   val convert: (String, Type) -> Any? = ConversionService::convert) : ConfigProvider() {
     @Suppress("UNCHECKED_CAST")
     override fun <N : Any> fetch(key: String, type: Type): N? {
-        val fullKey = prefix?.let { "$it.$key" } ?: key
+        val fullKey = (prefix?.let { "$it/$key" } ?: key).replace(separator, "/")
         val request = GetParameterRequest().withName(fullKey).withWithDecryption(true)
         return try {
             ssm.getParameter(request)?.parameter?.value?.trim()?.let { convert(it, type) as N }
