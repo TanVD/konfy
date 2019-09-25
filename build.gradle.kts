63399ea65ba8eb3468894025f8904a615136a4f4
Vladislav.Tankov <vdtankov@gmail.com>
2019-09-25 23:30:38 +0300

    Release version 0.1.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 group = "tanvd.konfy"
-version = "0.1.15-SNAPSHOT"
+version = "0.1.15"
 
 plugins {
     id("tanvd.kosogor") version "1.0.7" apply true
