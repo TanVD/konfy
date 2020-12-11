09b99652a8c56998851ff8c0ec9c30fabb054130
Vladislav.Tankov <vladislav.tankov@jetbrains.com>
2020-12-11 13:44:46 +0300

    Update to Kotlin 1.4.20 and prepare for the release of 0.1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 group = "tanvd.konfy"
-version = "0.1.17"
+version = "0.1.18"
 
 plugins {
-    id("tanvd.kosogor") version "1.0.9" apply true
-    id("io.gitlab.arturbosch.detekt") version ("1.8.0") apply true
-    kotlin("jvm") version "1.3.72" apply false
+    id("tanvd.kosogor") version "1.0.10" apply true
+    id("io.gitlab.arturbosch.detekt") version ("1.15.0-RC1") apply true
+    kotlin("jvm") version "1.4.20" apply false
 }
 
 
@@ -24,8 +24,8 @@ subprojects {
     tasks.withType<KotlinJvmCompile> {
         kotlinOptions {
             jvmTarget = "1.8"
-            languageVersion = "1.3"
-            apiVersion = "1.3"
+            languageVersion = "1.4"
+            apiVersion = "1.4"
         }
     }
 
