f0731984935c459d27bf147191e55064550f4ab7
Andrey.Tarashevskiy <andrey.tarashevskiy@jetbrains.com>
2022-10-20 14:17:58 +0300

    Move to 0.1.21-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import tanvd.kosogor.proxy.publishJar
 
 group = "tanvd.konfy"
-version = "0.1.20"
+version = "0.1.21-SNAPSHOT"
 
 plugins {
     id("tanvd.kosogor") version "1.0.16" apply true
