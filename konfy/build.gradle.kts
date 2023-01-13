0b2482c0adf4961a3bb3124cf0bd478f588a95bb
Iurii Nikolaev <nvyyvh@gmail.com>
2023-01-13 12:33:08 +0200

    Implementing cache invalidating in CachingProvider

--- konfy/build.gradle.kts
+++ konfy/build.gradle.kts
@@ -4,4 +4,5 @@ version = rootProject.version
 dependencies {
     api(kotlin("stdlib"))
     api(kotlin("reflect"))
+    api("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 }
--- konfy/src/main/kotlin/tanvd/konfy/provider/CachingProvider.kt
+++ konfy/src/main/kotlin/tanvd/konfy/provider/CachingProvider.kt
@@ -1,22 +1,31 @@
 package tanvd.konfy.provider
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import java.lang.reflect.Type
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.time.Duration
 
 /** ConfigProvider that caches all config values. */
-class CachingProvider(private val provider: ConfigProvider) : ConfigProvider() {
-    private object Null
+class CachingProvider(private val provider: ConfigProvider, private val invalidationPeriod: Duration = Duration.INFINITE) : ConfigProvider() {
+    class CacheEntry(val mark: Instant, val value: Any)
 
-    private val cache = ConcurrentHashMap<String, Any>()
+    private val cache = ConcurrentHashMap<String, CacheEntry>()
 
     @Suppress("UNCHECKED_CAST")
     override fun <N : Any> fetch(key: String, type: Type): N? {
-        val res = cache[key] ?: synchronized(key.intern()) {
-            cache.getOrPut(key) {
-                provider.tryGet(key, type) ?: Null
+        val value = cache[key]?.value ?: synchronized(key.intern()) {
+            val entry = cache[key]
+            if (entry == null || Clock.System.now().minus(entry.mark) > invalidationPeriod) {
+                val value = provider.tryGet<N>(key, type)
+                if (value != null) {
+                    cache[key] = CacheEntry(Clock.System.now(), value)
+                }
+                value
+            } else {
+                entry.value
             }
         }
-        if (res == Null) return null
-        return res as N?
+        return value as N?
     }
 }
