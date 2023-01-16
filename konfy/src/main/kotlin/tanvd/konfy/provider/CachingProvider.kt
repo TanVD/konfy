dca78aba74703702e120ca27528afe66715c15c4
Iurii Nikolaev <nvyyvh@gmail.com>
2023-01-16 16:00:02 +0200

    Reimplementing cache invalidation with background coroutine

--- konfy/src/main/kotlin/tanvd/konfy/provider/CachingProvider.kt
+++ konfy/src/main/kotlin/tanvd/konfy/provider/CachingProvider.kt
@@ -1,31 +1,48 @@
 package tanvd.konfy.provider
 
+import kotlinx.coroutines.*
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import java.lang.reflect.Type
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.CoroutineContext
 import kotlin.time.Duration
 
 /** ConfigProvider that caches all config values. */
-class CachingProvider(private val provider: ConfigProvider, private val invalidationPeriod: Duration = Duration.INFINITE) : ConfigProvider() {
-    class CacheEntry(val mark: Instant, val value: Any)
+class CachingProvider(
+    private val provider: ConfigProvider,
+    private val lifetime: Duration = Duration.INFINITE,
+    private val period: Duration = lifetime,
+    private val context: CoroutineContext = Dispatchers.Default
+) : ConfigProvider() {
+    class CacheEntry(val mark: Instant, val value: Any?)
 
     private val cache = ConcurrentHashMap<String, CacheEntry>()
 
-    @Suppress("UNCHECKED_CAST")
-    override fun <N : Any> fetch(key: String, type: Type): N? {
-        val value = cache[key]?.value ?: synchronized(key.intern()) {
-            val entry = cache[key]
-            if (entry == null || Clock.System.now().minus(entry.mark) > invalidationPeriod) {
-                val value = provider.tryGet<N>(key, type)
-                if (value != null) {
-                    cache[key] = CacheEntry(Clock.System.now(), value)
-                }
-                value
-            } else {
-                entry.value
+    init {
+        if (lifetime != Duration.INFINITE) {
+            runInvalidator()
+        }
+    }
+
+    private fun runInvalidator() {
+        CoroutineScope(context + CoroutineExceptionHandler { _, _ ->
+            runInvalidator()
+        }).launch {
+            while (currentCoroutineContext().isActive) {
+                val now = Clock.System.now()
+                cache.entries.removeIf { (_, entry) -> entry.mark.plus(lifetime) < now }
+                delay(period)
             }
         }
-        return value as N?
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <N : Any> fetch(key: String, type: Type): N? {
+        val now = Clock.System.now()
+        return cache.computeIfAbsent(key) {
+            val value = provider.tryGet<N>(it, type)
+            CacheEntry(now, value)
+        }.value as N?
     }
 }
